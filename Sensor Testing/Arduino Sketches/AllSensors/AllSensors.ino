c6c65d3cb59bbc809f99de9492b7e57b3a94fce0
Rehan <rehankshah14@gmail.com>
2018-04-17 19:41:43 -0700

    Sensor fusion completed

--- Sensor Testing/Arduino Sketches/AllSensors/AllSensors.ino
+++ Sensor Testing/Arduino Sketches/AllSensors/AllSensors.ino
@@ -5,7 +5,15 @@
 #define VREF 5.0
 #define SCOUNT  30
 #define LIGHTSENSOR 0 // Analog Input 0
+#define SensorPin A2  // pH meter on Analog Input 2
+#define Offset 0.00  // deviation compensate for pH
+#define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40 // times of collection
 
+int pHArray[ArrayLenth]; // Store the average value of the sensor feedback
+int pHArrayIndex=0;
 int analogBuffer[SCOUNT];
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
@@ -18,7 +26,7 @@ void setup()
   Serial.begin(9600); // Use 115200?
   dht.begin();
   pinMode(TDSSENSORPIN,INPUT);
-
+  pinMode(LED,OUTPUT);
 }
 
 void loop()
@@ -26,6 +34,7 @@ void loop()
   light();
   temp();
   tds();
+  pH();
 }
 
 int light()
@@ -123,4 +132,71 @@ int getMedianNum(int bArray[], int iFilterLen)
       else
   bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
       return bTemp;
+}
+
+float pH()
+{
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+  Serial.print("Voltage:");
+        Serial.print(voltage,2);
+        Serial.print("    pH value: ");
+  Serial.println(pHValue,2);
+        digitalWrite(LED,digitalRead(LED)^1);
+        printTime=millis();
+  delay(1000);
+  return pHValue;
+  }
+  return -1;
+}
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
 }
